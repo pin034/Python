cd02d7446937b09e2390e197f661100829e5503d
pin034 <60174760+pin034@users.noreply.github.com>
2020-02-06 09:40:39 +0700

    Create камень ножницы бумага.py

---
+++ игры/камень ножницы бумага.py
@@ -0,0 +1,22 @@
+print("Камень ножницы бумага")
+from random import *
+b = int(input("Введите число от 1 до 3, 1 - камень, 2 - ножницы, 3 - бумага"))
+a = randint(1,3)
+print(a)
+first = 1
+second = 2
+third = 3
+if b == first and a == third:
+    print("Вы проиграли!")
+elif b == second and a == third:
+    print("Вы выйграли!")
+elif b == first and a == second:
+    print("Вы выйграли!")
+elif b == third and a == second:
+    print("Вы проиграли!")
+elif b == second and a ==first:
+    print("Вы проиграли!")
+elif b == second and a == third:
+    print("Вы выйграли!")
+else:
+	print("Ничья")
