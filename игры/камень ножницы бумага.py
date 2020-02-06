3b5a8fe5da13cc15211b6526e118b70eb43de302
pin034 <60174760+pin034@users.noreply.github.com>
2020-02-06 11:34:41 +0700

    Update камень ножницы бумага.py

--- игры/камень ножницы бумага.py
+++ игры/камень ножницы бумага.py
@@ -7,16 +7,22 @@ first = 1
 second = 2
 third = 3
 if b == first and a == third:
+    print("Бумага")
     print("Вы проиграли!")
 elif b == second and a == third:
+    print("Бумага")
     print("Вы выйграли!")
 elif b == first and a == second:
+    print("Ножницы")
     print("Вы выйграли!")
 elif b == third and a == second:
+    print("Ножницы")
     print("Вы проиграли!")
 elif b == second and a ==first:
+    print("Камень")
     print("Вы проиграли!")
 elif b == second and a == third:
+    print("Бумага")
     print("Вы выйграли!")
 else:
 	print("Ничья")
